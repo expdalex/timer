f51edac31bdb38a6166a7c1c3f63fbb1b6ff6e5e
expdalex <49870498+expdalex@users.noreply.github.com>
2019-04-29 16:04:57 +0200

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,70 @@
+function timer(deadline){
+     
+	function getTimeRemaining(endtime) {
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			seconds = Math.floor((t/1000) % 60),
+			minutes = Math.floor((t/1000/60) % 60),
+			hours = Math.floor((t/(1000*60*60)))-3; //+3 moscow
+
+		return {
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		function updateClock(){
+			let t = getTimeRemaining(endtime);
+			if ((''+t.seconds).length == 1 && (''+t.hours).length == 1 && (''+t.minutes).length == 1){
+				hours.textContent = '0' + t.hours;
+				minutes.textContent = '0' + t.minutes;
+				seconds.textContent = '0' + t.seconds;
+			}else if ((''+t.seconds).length == 1 && (''+t.hours).length == 1){
+				hours.textContent = '0' + t.hours;
+				minutes.textContent = t.minutes;
+				seconds.textContent = '0' + t.seconds;
+			} else if((''+t.minutes).length == 1 && (''+t.hours).length == 1){
+				minutes.textContent = '0' + t.minutes;
+				hours.textContent = '0' + t.hours;
+				seconds.textContent = t.seconds;
+			} else if((''+t.minutes).length == 1 && (''+t.seconds).length == 1){
+				minutes.textContent = '0' + t.minutes;
+				hours.textContent = t.hours;
+				seconds.textContent = '0' + t.seconds;
+			} else if ((''+t.hours).length == 1){
+				hours.textContent = '0' + t.hours;
+				minutes.textContent = t.minutes;
+				seconds.textContent = t.seconds;   
+			} else if((''+t.minutes).length == 1){
+				minutes.textContent = '0' + t.minutes;
+				hours.textContent = t.hours;
+				seconds.textContent = t.seconds;
+			} else if((''+t.seconds).length == 1) {
+				hours.textContent = t.hours;
+				minutes.textContent = t.minutes;
+				seconds.textContent = '0' + t.seconds;
+			}
+			else {
+				hours.textContent = t.hours;
+				minutes.textContent = t.minutes;
+				seconds.textContent = t.seconds;
+				console.log((''+t.seconds).length);
+			}      
+			if (t.total <= 0){
+				clearInterval(timeInterval);
+				hours.textContent = '00';
+				minutes.textContent = '00';
+				seconds.textContent = '00';
+			}
+		}
+	}
+	setClock('timer', deadline);
+}
